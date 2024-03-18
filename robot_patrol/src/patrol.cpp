569e7595f663111bb97dda0ea5451c4cb4435faa
Miguel Solis Segura <miguelsolissegura@outlook.com>
2024-03-18 23:43:43 +0000

    Read front objects

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -5,7 +5,7 @@ class Patrol : public rclcpp::Node {
 public:
     // Public methods
     Patrol() : rclcpp::Node("patrol") {
-        _subscription = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
             "/scan", 
             10, 
             std::bind(&Patrol::laserCallback, this, std::placeholders::_1));
@@ -14,10 +14,21 @@ private:
     // Private methods
     void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
         int size = static_cast<int>(msg->ranges.size());
-        RCLCPP_INFO(this->get_logger(), "The laser scans array has a size of: %i", size);
+        float max_value = static_cast<float>(msg->range_max);
+        float far_object = 0;
+        for (int i = 0; i < size / 2; i++) {
+            if (max_value >= msg->ranges[i] && msg->ranges[i] > far_object) {
+                far_object = msg->ranges[i];
+                direction_ = static_cast<float>(i)/size * 360;
+            }
+        }
+        RCLCPP_INFO(this->get_logger(), "The furthest object is at: %.2f m", far_object);
+        RCLCPP_INFO(this->get_logger(), "The direction is : %.2f degres", direction_);
     }
+
     // Private attributes
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr _subscription;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+    float direction_;
 };
 
 int main(int argc, char ** argv) {
