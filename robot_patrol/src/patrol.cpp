4959b2639d4eba9f3b310d51f096051c8c421db1
Miguel Solis Segura <miguelsolissegura@outlook.com>
2024-03-18 22:19:14 +0000

    Create subscription to /scan messages

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,9 +1,29 @@
-#include "rclcpp/node.hpp"
 #include <rclcpp/rclcpp.hpp>
+#include "sensor_msgs/msg/laser_scan.hpp"
 
-class Patrol : rclcpp::Node {
+class Patrol : public rclcpp::Node {
 public:
-
+    // Public methods
+    Patrol() : rclcpp::Node("patrol") {
+        _subscription = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "/scan", 
+            10, 
+            std::bind(&Patrol::laserCallback, this, std::placeholders::_1));
+    }
 private:
-
+    // Private methods
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+        int size = static_cast<int>(msg->ranges.size());
+        RCLCPP_INFO(this->get_logger(), "The laser scans array has a size of: %i", size);
+    }
+    // Private attributes
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr _subscription;
 };
+
+int main(int argc, char ** argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<Patrol>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
