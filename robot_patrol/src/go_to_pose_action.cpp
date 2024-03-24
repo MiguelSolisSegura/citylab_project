007a53f771fe21a5556b0128f01ffd63d04b2f44
Miguel Solis Segura <miguelsolissegura@outlook.com>
2024-03-24 01:44:41 +0000

    Implement absolute orientation control

--- robot_patrol/src/go_to_pose_action.cpp
+++ robot_patrol/src/go_to_pose_action.cpp
@@ -1,23 +1,37 @@
+#include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "geometry_msgs/msg/detail/twist_with_covariance__struct.hpp"
+#include "nav_msgs/msg/detail/odometry__struct.hpp"
 #include "rclcpp/executors/multi_threaded_executor.hpp"
+#include "rclcpp/publisher.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp/subscription.hpp"
+#include "rclcpp/utilities.hpp"
 #include "rclcpp_action/create_server.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "geometry_msgs/msg/pose2_d.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp_action/server.hpp"
 #include "rclcpp_action/server_goal_handle.hpp"
 #include "rclcpp_action/types.hpp"
 #include "robot_patrol/action/go_to_pose.hpp"
+#include <cmath>
 #include <functional>
 #include <memory>
+#include <thread>
+#include <cmath>
 
 using Pose2D = geometry_msgs::msg::Pose2D;
+using Twist = geometry_msgs::msg::Twist;
 using GoToPose = robot_patrol::action::GoToPose;
+using Odometry = nav_msgs::msg::Odometry;
 using GoalHandleGoToPose = rclcpp_action::ServerGoalHandle<GoToPose>;
 using namespace std::placeholders;
 
 class GoToPoseServer : public rclcpp::Node {
 public:
     GoToPoseServer() : rclcpp::Node::Node("go_to_pose_action") {
+        this->subscription_ = this->create_subscription<Odometry>("/odom", 10, std::bind(&GoToPoseServer::odom_callback, this, _1));
         this->action_server_ = rclcpp_action::create_server<GoToPose>(
             this,
             "/go_to_pose",
@@ -25,12 +39,18 @@ public:
             std::bind(&GoToPoseServer::handle_cancel, this, _1),
             std::bind(&GoToPoseServer::handle_accepted, this, _1)
         );
-    RCLCPP_INFO(this->get_logger(), "Launched /go_to_pose action server.");
+        this->publisher_ = this->create_publisher<Twist>("/cmd_vel", 10);
+        
+        RCLCPP_INFO(this->get_logger(), "Launched /go_to_pose action server.");
     }
 private:
     // Attributes
     Pose2D desired_pos_;
+    Pose2D current_pos_;
     rclcpp_action::Server<GoToPose>::SharedPtr action_server_;
+    rclcpp::Subscription<Odometry>::SharedPtr subscription_;
+    rclcpp::Publisher<Twist>::SharedPtr publisher_;
+
     // Methods
     rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const GoToPose::Goal> goal) {
         RCLCPP_INFO(this->get_logger(), 
@@ -44,17 +64,60 @@ private:
         return rclcpp_action::CancelResponse::ACCEPT;
     }
     void handle_accepted(const std::shared_ptr<GoalHandleGoToPose> goal_handle) {
-        RCLCPP_INFO(this->get_logger(), "Excecuting goal");
         const auto goal = goal_handle->get_goal();
+        desired_pos_.x = goal->goal_pos.x;
+        desired_pos_.y = goal->goal_pos.y;
+        desired_pos_.theta = goal->goal_pos.theta;
+        std::thread{std::bind(&GoToPoseServer::execute, this, _1), goal_handle}.detach();
+        
+    }
+    void execute(const std::shared_ptr<GoalHandleGoToPose> goal_handle) {
+        RCLCPP_INFO(this->get_logger(), "Executing goal: x: %.2f, y: %.2f, theta: %.2f",
+            desired_pos_.x, desired_pos_.y, desired_pos_.theta);
+
+        // Orientation
+        float angle_delta = INFINITY;
+        float goal_direction = computeVectorAngle(desired_pos_.x, desired_pos_.y);
+
+        // Feedback
         auto feedback = std::make_shared<GoToPose::Feedback>();
         auto &current_distance = feedback->current_pos;
-        current_distance.x = goal->goal_pos.x;
-        current_distance.y = goal->goal_pos.y;
-        current_distance.theta = goal->goal_pos.theta;
-        goal_handle->publish_feedback(feedback);
+        current_distance.x = current_pos_.x ;
+        current_distance.y = current_pos_.y;
+        current_distance.theta = current_pos_.theta;
+
+        // Rotate to target
+        auto vel_msg = Twist();
+        
+        while (std::abs(angle_delta) > M_PI / 360) {
+            RCLCPP_INFO(this->get_logger(), "Goal direction: %.2f", goal_direction);
+            RCLCPP_INFO(this->get_logger(), "Current orientation: %.2f", current_pos_.theta);
+            angle_delta = goal_direction - current_pos_.theta;
+            RCLCPP_INFO(this->get_logger(), "Angle delta: %.2f", angle_delta);
+            vel_msg.angular.z = angle_delta;
+            publisher_->publish(vel_msg);
+            goal_handle->publish_feedback(feedback);
+        }
+
+        // Send result
         auto result = std::make_shared<GoToPose::Result>();
         result->status = true;
         goal_handle->succeed(result);
+    }
+    void odom_callback(const Odometry::SharedPtr msg) {
+        current_pos_.x = msg->pose.pose.position.x;
+        current_pos_.y = msg->pose.pose.position.y;
+        current_pos_.theta = computeRotationAngleZ(msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+    }
+    float computeRotationAngleZ(const float z, const float w) {
+        // Compute the rotation angle in radians
+        float thetaRadians = 2 * atan2(z, w);
+        return thetaRadians;
+    }
+    float computeVectorAngle(const float x, const float y) {
+        // Compute the angle in radians
+        float thetaRadians = atan2(y, x);
+        return thetaRadians;
     }
 };
 
